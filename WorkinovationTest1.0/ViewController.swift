a10c9efcbb7a22ffa18f2a7b60291e1355388ab2
近井昭夫 <chikai2@me.com>
2016-06-02 17:10:34 +0900

    Initial Commit

---
+++ WorkinovationTest1.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorkinovationTest1.0
+//
+//  Created by Akio Chikai on 2016/06/02.
+//  Copyright © 2016年 Akio Chikai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
